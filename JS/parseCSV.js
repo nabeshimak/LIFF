699432d091777d54c2e1c897025a8095afab34a9
Yuta <57943939+KY-1998@users.noreply.github.com>
2022-10-01 00:55:08 +0900

    Update parseCSV.js

--- JS/parseCSV.js
+++ JS/parseCSV.js
@@ -1,23 +1,27 @@
-
 function parseCSV(data) {
     // CSVを配列で読み込む
     const csv = $.csv.toArrays(data);
 
     $(csv).each(function (i) {
-        if(this.length > 1 && i > 0){
+        if ($(csv).length > 1 && i > 0) {
+            console.log("$(csv).length: " + $(csv).length);
+            const columnLen = $(this).length;
             let text = '';
             $(this).each(function (j) {
-                if (j == this.length - 1) {
+                console.log(j);
+                if (j >= columnLen - 1) {
                     text += this;
                 } else {
                     text += this + '/';
                 }
+                console.log(text);
             });
             //追加するとき
             const selectboxId = "#class_type";
             $(selectboxId).append($('<option>').html(text).val(text));
-        } else {
-            alert("CSV Error: Invalid CSV file contents. Please confirm your 'CSV file'");
+        }
+        else if ($(csv).length < 1) {
+            alert("CSV Error: Invalid CSV file contents. Please confirm your 'CSV file'. (No data)");
         }
     });
 }
